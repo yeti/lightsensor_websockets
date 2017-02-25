47df6c9b10a8132ead08560e67baf1cdb1112642
paolopaolopaolo <dean@yeti.co>
2017-02-25 06:16:18 -0500

    Add PubSub type files AND Nginx configs

--- browser_subscriber.py
+++ browser_subscriber.py
@@ -2,26 +2,26 @@ import asyncio
 from aioredis import create_connection, Channel
 import websockets
 
-async def subscribe_to_redis():
+async def subscribe_to_redis(path):
 	conn = await create_connection(('localhost', 6379))
-	channel = Channel('lightlevel', is_pattern=False)
+	channel = Channel('lightlevel{}'.format(path), is_pattern=False)
 	await conn.execute_pubsub('subscribe', channel)
 	return channel, conn
 	
 
 async def browser_server(websocket, path):
-	channel, conn = await subscribe_to_redis()
+	channel, conn = await subscribe_to_redis(path)
 	try:
 		while True:
 			message = await channel.get()
 			await websocket.send(message.decode('utf-8'))
-	finally:
+	except websockets.exceptions.ConnectionClosed:
 		await conn.execute_pubsub('unsubscribe', channel)
 		conn.close()
 
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
 	loop.set_debug(True)
-	ws_server = websockets.serve(browser_server, 'localhost', 8000)
+	ws_server = websockets.serve(browser_server, 'localhost', 8767)
 	loop.run_until_complete(ws_server)
 	loop.run_forever()
--- pi_publisher.py
+++ pi_publisher.py
@@ -2,16 +2,17 @@ import asyncio
 from aioredis import create_connection, Channel
 import websockets
 
-async def publish_to_redis(msg):
+async def publish_to_redis(msg, path):
 	conn = await create_connection(('localhost', 6379))
-	await conn.execute('publish', 'lightlevel', msg)
+	await conn.execute('publish', 'lightlevel{}'.format(path), msg)
 	conn.close()
 
 async def server(websocket, path):
 	try:
 		while True:
 			message = await websocket.recv()
-			await publish_to_redis(message)
+			await publish_to_redis(message, path)
+			await asyncio.sleep(1)
 	except websockets.exceptions.ConnectionClosed:
 		print('Connection Closed!')
 
@@ -19,6 +20,6 @@ async def server(websocket, path):
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
 	loop.set_debug(True)
-	ws_server = websockets.serve(server, 'localhost', 8002)
+	ws_server = websockets.serve(server, 'localhost', 8765)
 	loop.run_until_complete(ws_server)
 	loop.run_forever()
